4bbc9d1de4b7e983fccf5cde65c248253c241871
Morz75 <alexandermarinen@gmail.com>
2022-04-26 21:18:17 +0600

    1.1.6

--- GAMES/Wordle/wordle.js
+++ GAMES/Wordle/wordle.js
@@ -1,6 +1,6 @@
 let dictionary = [];
 let words = [];
-let board;
+let board = [];
 
 async function loadGame() {
 	let filePath = QuintOS.dir + '/words5.txt';
@@ -20,7 +20,6 @@ async function loadGame() {
 			dictionary.push(line[j]);
 		}
 	}
-	log(dictionary);
 
 	startGame();
 }
@@ -28,11 +27,82 @@ async function loadGame() {
 loadGame();
 
 /* Display all the boxes for the letters */
-function displayBoxes() {}
+function displayBoxes() {
+	let boxes = '';
+	for (let i = 0; i < 6; i++) {
+		boxes += '┌─┐'.repeat(5) + '\n';
+		boxes += '│ │'.repeat(5) + '\n';
+		boxes += '└─┘'.repeat(5) + '\n';
+	}
+
+	text(boxes, 2, 2);
+}
+
+function displayInfo() {
+	let row = 10;
+	textRect(row, 20, 3, 3, 'solid');
+	text('letter is not found in word', row, 24);
+	row += 3;
+	textRect(row, 20, 3, 3, 'outline');
+	text('letter is in the word', row, 24);
+	row += 3;
+	textRect(row, 20, 3, 3, 'dashed');
+	text('letter is in the correct position', row, 24, 14);
+}
 
 async function startGame() {
 	/* pick new word */
+	let wordIndex = random(0, words.length);
+	wordIndex = Math.round(wordIndex);
+	log(wordIndex);
+
+	let word = words[wordIndex];
+	log(word);
 
 	displayBoxes();
-	await prompt('Guess the word!', 3, 18, 20);
+	displayInfo();
+
+	let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	let validGuesses = 0;
+	while (validGuesses < 6) {
+		text(alpha, 21, 2);
+		let guess = await prompt('Guess the word!', 2, 18, 20);
+		// convert to all uppercase
+		guess = guess.toUpperCase();
+
+		if (guess.length > 5) {
+			await alert('The word is too big!', 2, 18, 20);
+		} else if (guess.length < 5) {
+			await alert('The word is too small!', 2, 18, 20);
+		} else if (dictionary.includes(guess) == false) {
+			await alert('The word is not in the dictionary.', 2, 18, 20);
+		} else {
+			let row = 2 + validGuesses * 3;
+			await eraseRect(row, 2, 15, 3);
+
+			for (let i = 0; i < 5; i++) {
+				let letter = guess[i];
+				if (letter == word[i]) {
+					textRect(row, 2 + i * 3, 3, 3, 'dashed');
+				} else if (word.includes(letter) == true) {
+					textRect(row, 2 + i * 3, 3, 3, 'outline');
+				} else {
+					textRect(row, 2 + i * 3, 3, 3, 'solid');
+					alpha = alpha.replace(letter, ' ');
+				}
+
+				text(letter, row + 1, 3 + i * 3);
+			}
+			validGuesses++;
+			if (guess == word) {
+				await alert('You won!', 2, 18, 20);
+				break;
+			} else if (validGuesses >= 6) {
+				await alert('You lost(', 2, 18, 20);
+			}
+		}
+	}
+
+	startGame();
 }
